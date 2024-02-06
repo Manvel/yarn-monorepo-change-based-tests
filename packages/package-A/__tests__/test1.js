75f2cc572d9250bab2740c53f8baad5974c4be37
Manvel Saroyan <saroyanm@gmail.com>
2024-02-06 19:56:36 +0400

    added workspaces with tests

---
+++ packages/package-A/__tests__/test1.js
@@ -0,0 +1,5 @@
+describe("package-A test1", () => {
+  it("testcase 1.", async () => {
+    expect(1).toBe(1);
+  });
+});
---
+++ packages/package-B/__tests__/test1.js
@@ -0,0 +1,5 @@
+describe("package-B test1", () => {
+  it("testcase 1.", async () => {
+    expect(1).toBe(1);
+  });
+});
