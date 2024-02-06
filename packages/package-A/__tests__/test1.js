7ef39f9f7cd69a4f5501597253a796a9e4bd923f
Manvel Saroyan <saroyanm@gmail.com>
2024-02-06 21:00:13 +0400

    use changedSince

--- packages/package-A/__tests__/test1.js
+++ packages/package-A/__tests__/test1.js
@@ -1,5 +1,7 @@
+const data = require("../data.json");
 describe("package-A test1", () => {
   it("testcase 1.", async () => {
-    expect(1).toBe(1);
+    // change 1
+    expect(data.paramA).toBe("value1");
   });
 });
